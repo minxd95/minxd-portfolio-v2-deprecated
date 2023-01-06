dab5d1fa22754d42f978cc086df02eb9dcc8a1e3
minxd95 <newobj95@gmail.com>
2023-01-06 20:03:08 +0900

    eslint, prettier 세팅

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,12 @@
+const path = require("path")
+
 module.exports = {
   babel: {
     presets: ["@emotion/babel-preset-css-prop"],
   },
-};
+  webpack: {
+    alias: {
+      "@": path.resolve("src/"),
+    },
+  },
+}
